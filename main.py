e6ecf1c0e6ec6ab426bea03cfabbcf213ab86108
ANT5X <egorkarl10@gmail.com>
2024-08-27 20:47:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-dict1 = {'key1':='value1'}
+# дані одного з читачів. Не змінювати!
+student_card = {'номер': '324', 'прізвище': 'Іванов'} 
+print("Ласкаво просимо!", student_card)
 
-print(dict1)
+
+
+while True:
+    cab = input("Особистий кабінет: 1 – взяти, 2 – повернути, 3 – додому")
+    if cab == '1':
+        pl = input("Введіть назву:")
+        student_card['борг'] = pl
+    elif cab == '2':
+        if 'борг' in student_card:
+            del student_card['борг']
+    elif cab == '3':
+        break
+    else:
+        print("Невірний вибір. Спробуйте ще раз.")
+    print("Картка читача:", student_card)
+
+print("Чекаємо на вас:", student_card)
